760ef4beb458b7a6426467c7fce72ea712bd9dac
bgasparotto <bruno.m.gasparotto@gmail.com>
2017-12-08 09:57:47 -0200

    Added more data for H2 initial load.

--- springboot-veggieburger/src/main/resources/data.sql
+++ springboot-veggieburger/src/main/resources/data.sql
@@ -13,3 +13,21 @@ VALUES
 (1, 'Bruno', '208 Paulista Avenue, Sao Paulo - SP. Brazil'),
 (2, 'John', '1400 Faria Lima, Sao Paulo - SP. Brazil'),
 (3, 'Rose', '300 Casa Verde Avenue, Sao Paulo - SP. Brazil');
+
+INSERT INTO purchase
+(id, date, total_value, customer_id)
+VALUES
+(1, '2017-12-01 07:01:01', 80, 1),
+(2, '2017-12-01 07:02:01', 80, 1),
+(3, '2017-12-01 07:03:01', 80, 1),
+(4, '2017-12-01 07:04:01', 80, 2),
+(5, '2017-12-01 07:05:01', 80, 2);
+
+INSERT INTO purchase_items
+(purchase_id, items_id)
+VALUES
+(1, 1), (1, 2), (1,3),
+(2, 1), (2, 2),
+(3,1),(3, 1), (3, 2), (3,2),
+(4, 1),
+(5, 1), (5,2), (5, 3), (5, 4), (5,5);
