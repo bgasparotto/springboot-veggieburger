3874dee2e706061cd971346196d687af80667b29
bgasparotto <bruno.m.gasparotto@gmail.com>
2017-12-08 10:27:08 -0200

    Added the customer details view.

--- springboot-veggieburger/src/main/java/com/bgasparotto/springboot/veggieburger/web/CustomerController.java
+++ springboot-veggieburger/src/main/java/com/bgasparotto/springboot/veggieburger/web/CustomerController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -26,5 +27,10 @@ public class CustomerController {
 	public ModelAndView list() {
 		List<Customer> customers = repository.findAll();
 		return new ModelAndView("customers/list", "customers", customers);
+	}
+	
+	@GetMapping("{id}")
+	public ModelAndView view(@PathVariable("id") Customer customer) {
+		return new ModelAndView("customers/view", "customer", customer);
 	}
 }
